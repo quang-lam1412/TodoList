0b3d708f50ea7795bc3aca4fef8cf9b6be752658
Truong Quang Lam <truongquanglam1627@gmail.com>
2025-11-30 21:56:39 +0700

    first commit

---
+++ my-app/src/components/TaskInput/TaskInput.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+import styles from './taskInput.module.scss'
+import { Todo } from '../../@types/todo.type'
+import { TodoTypes } from '../../PropTypes/todo.proptype'
+
+interface TaskInputProps {
+  addTodo: (name: string) => void
+  editTodo: (name: string) => void
+  currentTodo: Todo | null
+  finishEditTodo: () => void
+}
+export default function TaskInput(props: TaskInputProps) {
+  const { addTodo, currentTodo, editTodo, finishEditTodo } = props
+  const [name, setName] = useState<string>('')
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (currentTodo) {
+      finishEditTodo()
+      if (name) setName('')
+    } else {
+      addTodo(name)
+      setName('')
+    }
+  }
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    if (currentTodo) {
+      editTodo(value)
+    } else {
+      setName(value)
+    }
+  }
+  return (
+    <div className='mb-2'>
+      <h1 className={styles.title1}>
+        Trương Diễm Hạnh <span className={styles.cat}>🐱</span>
+      </h1>
+
+      <h1 className={styles.title}>To do list typescript</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='caption goes here'
+          value={currentTodo ? currentTodo.name : name}
+          onChange={onChangeInput}
+        />
+        <button type='submit'>{currentTodo ? '✔️' : '➕'}</button>
+      </form>
+    </div>
+  )
+}
+TaskInput.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  editTodo: PropTypes.func.isRequired,
+  finishEditTodo: PropTypes.func.isRequired,
+  currentTodo: PropTypes.oneOfType([TodoTypes, PropTypes.oneOf([null])])
+}
---
+++ my-app/src/components/TodoList/TodoList.tsx
@@ -0,0 +1,111 @@
+import { useEffect, useState } from 'react'
+import TaskInput from '../TaskInput'
+import TaskList from '../TaskList'
+import styles from './todoList.module.scss'
+import { Todo } from '../../@types/todo.type'
+
+// interface HandNewTodos {
+//   (todos: Todo[]): Todo[]
+// }
+type HandNewTodos = (todos: Todo[]) => Todo[]
+const syncReactTolocal = (handNewTodos: HandNewTodos) => {
+  const todoString = localStorage.getItem('todos')
+  const todosObj: Todo[] = JSON.parse(todoString || '[]')
+  const newTodosObj = handNewTodos(todosObj)
+  localStorage.setItem('todos', JSON.stringify(newTodosObj))
+}
+export default function TodoList() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [currentTodo, setCurrentTodo] = useState<Todo | null>(null)
+  const doneTodos = todos.filter((todo) => todo.done)
+  const notDoneTodos = todos.filter((todo) => !todo.done)
+
+  useEffect(() => {
+    const todoString = localStorage.getItem('todos')
+    const todoObj: Todo[] = JSON.parse(todoString || '[]')
+    setTodos(todoObj)
+  }, [])
+  const addTodo = (name: string) => {
+    const todo: Todo = {
+      name,
+      done: false,
+      id: new Date().toISOString()
+    }
+    setTodos((prev) => [...prev, todo])
+    syncReactTolocal((todosObj: Todo[]) => [...todosObj, todo])
+  }
+  const handleDoneTodo = (id: string, done: boolean) => {
+    setTodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, done }
+        }
+        return todo
+      })
+    })
+  }
+  const startEditTodo = (id: string) => {
+    const findedTodo = todos.find((todo) => todo.id === id)
+    if (findedTodo) {
+      setCurrentTodo(findedTodo)
+    }
+  }
+
+  const editTodo = (name: string) => {
+    setCurrentTodo((prev) => {
+      if (prev) return { ...prev, name }
+      return null
+    })
+  }
+
+  const finishEditTodo = () => {
+    const handler = (todoObj: Todo[]) => {
+      return todoObj.map((todo) => {
+        if (todo.id === (currentTodo as Todo).id) {
+          return currentTodo as Todo
+        }
+        return todo
+      })
+    }
+    setTodos(handler)
+    setCurrentTodo(null)
+    syncReactTolocal(handler)
+  }
+  const deleteTodo = (id: string) => {
+    if (currentTodo) {
+      setCurrentTodo(null)
+    }
+    const handler = (todoObj: Todo[]) => {
+      const findedIndexTodo = todoObj.findIndex((todo) => todo.id === id)
+      if (findedIndexTodo > -1) {
+        const result = [...todoObj]
+        result.splice(findedIndexTodo, 1)
+        return result
+      }
+      return todoObj
+    }
+    setTodos(handler)
+    syncReactTolocal(handler)
+  }
+  return (
+    <div className={styles.todoList}>
+      <div className={styles.todoListContainer}>
+        <TaskInput addTodo={addTodo} currentTodo={currentTodo} editTodo={editTodo} finishEditTodo={finishEditTodo} />
+        <TaskList
+          doneTaskList={false}
+          todos={notDoneTodos}
+          handleDoneTodo={handleDoneTodo}
+          startEditTodo={startEditTodo}
+          deleteTodo={deleteTodo}
+        />
+        <TaskList
+          doneTaskList
+          todos={doneTodos}
+          handleDoneTodo={handleDoneTodo}
+          startEditTodo={startEditTodo}
+          deleteTodo={deleteTodo}
+        />
+      </div>
+    </div>
+  )
+}
